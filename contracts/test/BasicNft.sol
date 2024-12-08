bca63f6797a08724172dfa2d21021772790876c7
yommexg <hydrokidb@gmail.com>
2024-12-08 14:21:53 +0000

    Added mint and list nft scripts

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -10,19 +10,25 @@ contract BasicNft is ERC721 {
 
     uint256 private s_tokenCounter;
 
+    event DogMinted(uint256 indexed tokenId);
+
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public returns (uint256) {
+    function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        emit DogMinted(s_tokenCounter);
         s_tokenCounter += 1;
-        return s_tokenCounter;
     }
 
     function tokenURI(
-        uint256 /*tokenId*/
-    ) public pure override returns (string memory) {
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         return TOKEN_URI;
     }
 
